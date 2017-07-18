2bb35b1328664e72aa9c3a077c14a331c19c1ef9
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2017-07-18 01:35:00 -0700

    Camera, PlatformGenerator, Scene

--- CodeScifi/Assets/CameraFollow.cs
+++ CodeScifi/Assets/CameraFollow.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
 	Transform target;
+	Transform cameraContainer;
 	float posX;
 	float posY;
 	Vector2 velocity;
 	float smoothTimeX;
 	float smoothTimeY;
 	float offsetX;
+	float offsetY;
 
 	// Use this for initialization
 	void Start () {
 		target = GameObject.Find("Hero2 (1)").transform;
+		cameraContainer = transform.parent;
 		smoothTimeX = 0f;
 		smoothTimeY = .2f;
-		offsetX = -4f;
+		offsetX = -12f;
+		offsetY = 6f;
+		cameraContainer.position = new Vector3(cameraContainer.position.x, cameraContainer.position.y + offsetY, cameraContainer.position.z);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        posX = Mathf.SmoothDamp(transform.position.x, target.transform.position.x, ref velocity.x, smoothTimeX);
-        transform.position = new Vector3(posX - offsetX, transform.position.y, transform.position.z);
-        
+        posX = Mathf.SmoothDamp(cameraContainer.position.x, target.transform.position.x, ref velocity.x, smoothTimeX);
+		cameraContainer.position = new Vector3(posX - offsetX, cameraContainer.position.y, cameraContainer.position.z);
+        //
     }
 }
