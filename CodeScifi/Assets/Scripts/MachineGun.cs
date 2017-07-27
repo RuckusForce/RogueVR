667d6bb19c4fea71c960f3b4bc54e64228c9b219
xWRIOTx <blue25th@gmail.com>
2017-07-26 19:26:07 -0700

    7/26/2017 changes
    Completed Machine  and Shotgun script. Shotgun script is a little dirty
    but it functions the way I wanted to. I noticed and realized that I need
    to implement switching weapons when picking up the power ups. On the
    images I sent you, it displays what is new and where to look for it.

---
+++ CodeScifi/Assets/Scripts/MachineGun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGun : MonoBehaviour
+{
+    public GameObject rBullet = null;
+    private Rigidbody rBodyRef = null;
+    
+    private float fireRate = 0.1f;
+    private float nextFire;
+
+    private void Awake()
+    {
+        rBodyRef = rBullet.GetComponent<Rigidbody>();
+    } 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PewPew();
+    }
+
+    public void PewPew()
+    {
+        if (Input.GetKey(KeyCode.Mouse1) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(rBullet, transform.position + (transform.right * 1), transform.rotation);
+            Rigidbody rb = rBullet.GetComponent<Rigidbody>();
+            rb.velocity = transform.right * 80;
+        }
+    }
+
+    
+}
