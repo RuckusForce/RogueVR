7f1ebe32a96225dd7cc7c65a23106794259e5ab1
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2017-08-14 08:50:22 -0700

    Implemented DeathAnim
    -Runtime has bugs
    --Only sometimes recognizes Bullet Collider2D
    --Collision still triggers the old Death Canvas

--- CodeScifi/Assets/GAA/Scripts/TimeKeeperScript.cs
+++ CodeScifi/Assets/GAA/Scripts/TimeKeeperScript.cs
@@ -21,7 +21,7 @@ public class TimeKeeperScript : MonoBehaviour {
 		platformGenerators = GameObject.Find("PlatformGenerators");
 		platformGeneratorArray = platformGenerators.GetComponentsInChildren<Transform>();
 		groundHeight = -4f;
-		levelTimeLimit = 4f;
+		levelTimeLimit = 10f;
 	}
 
 	// Update is called once per frame
