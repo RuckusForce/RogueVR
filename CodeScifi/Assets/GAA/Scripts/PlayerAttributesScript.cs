2646b0e9dc59cc3c88daf340d1489bfaf04c7672
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2017-08-15 00:17:11 -0700

    Revert "Revert "Implemented DeathAnim""
    This reverts commit 07a9b55560efccac7b809a4ee8b3a76a6bd1eef4.

--- CodeScifi/Assets/GAA/Scripts/PlayerAttributesScript.cs
+++ CodeScifi/Assets/GAA/Scripts/PlayerAttributesScript.cs
@@ -63,8 +63,8 @@ public class PlayerAttributesScript : MonoBehaviour
         //shield.Initialize(shieldBar);
 
         gameController = GameObject.Find("GameController");
-        //retryGame = gameController.GetComponent<RetryGame>();
-    }
+		retryGame = gameController.GetComponent<RetryGame>();
+	}
 
     void Start() {
         invincible = false;
